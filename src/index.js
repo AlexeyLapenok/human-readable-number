7f525768af1c02f4809e2efb81fe10baf7d50ca9
LapenokAlexey <aleks.lapenok@gmail.com>
2021-08-04 12:38:54 +0300

    refactor: some changes

--- src/index.js
+++ src/index.js
@@ -11,19 +11,16 @@ module.exports = function toReadable(number) {
         word = wordDozens + ' ' + arr[String(number)[1]];
         word === wordDozens + ' ' + 'zero' ? word = dozens[String(number)[0]] : '';
     } else if (number >= 100 && number < Number(String(number)[0] + '10')) {
-        const wordDozens = dozens[String(number)[1]];
         word = arr[String(number)[0]] + ' hundred ' + arr[String(number)[2]];
-        word === arr[String(number)[0]] + ' hundred ' + 'zero' ? word = arr[String(number)[0]] + ' hundred' : '';
+        word === arr[String(number)[0]] + ' hundred ' + 'zero'
+            ? word = arr[String(number)[0]] + ' hundred' : '';
     } else if (number >= 100 && number < Number(String(number)[0] + '20')) {
-        const wordDozens = dozens[String(number)[1]];
         word = arr[String(number)[0]] + ' hundred ' + arr[String(number).slice(1, 3)];
-        word === arr[String(number)[0]] + ' hundred ' + 'zero' ? word = arr[String(number)[0]] + ' hundred' : '';
     } else if (number >= Number(String(number)[0] + '20')) {
         const wordDozens = dozens[String(number)[1]];
         word = arr[String(number)[0]] + ' hundred ' + wordDozens + ' ' + arr[String(number)[2]];
-        word === arr[String(number)[0]] + ' hundred ' + wordDozens + ' ' + 'zero' ? word = arr[String(number)[0]] + ' hundred ' + wordDozens : '';
+        word === arr[String(number)[0]] + ' hundred ' + wordDozens + ' ' + 'zero'
+            ? word = arr[String(number)[0]] + ' hundred ' + wordDozens : '';
     }
-
-    console.log(word);
     return word;
 }
